bf5ddaa5dfced5953fde1d1682936dc34d704077
RahulAid <113744018+RahulAid@users.noreply.github.com>
2022-09-27 20:22:13 +0530

    Print a Day using Case Statement

---
+++ Day 05/Case Statement/DayWithCase.sh
@@ -0,0 +1,24 @@
+read -p "Enter a number between 1 and 7" a
+  case $a in
+		1)
+    echo "MONDAY"
+    ;;
+    2)
+    echo "TUESDAY"
+    ;;
+    3)
+    echo "WEDNESDAY"
+    ;;
+    4)
+    echo "THURSDAY"
+    ;;
+    5)
+		echo "FRIDAY"
+    ;;
+    6)
+    echo "SATURDAY"
+    ;;
+    7)
+		echo "SUNDAY"
+    ;;
+    esac
